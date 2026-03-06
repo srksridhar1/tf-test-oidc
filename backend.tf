d0e7b9bf20212610828a5ff43c5e2e5be6102cc4
Sridhar Kulkarni <sridhar.kulkarni@ey.com>
2026-03-05 19:39:15 -0600

    Four

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "srk-my-secure-tf-state"
     key            = "terraform/state"
-    region         = "eu-east-2"
+    region         = "us-east-2"
     encrypt        = true
   }
 }
